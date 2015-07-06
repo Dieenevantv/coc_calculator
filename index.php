f103dcbc54fa03fab9d3e23a2988cbfb0ccc07cd
Dieenevantv <dieenevantv@gmail.com>
2015-07-06 11:26:24 +0200

    veel basis velden toegevoegd
    nu eerst weer wat codeschool doen om basis op te halen

--- index.php
+++ index.php
@@ -1,18 +1,153 @@
 
-<table style="width:100%">
+<table border='1' style="width:50%">
   <tr>
-    <td>Jill</td>
-    <td>Smith</td> 
-    <td>50</td>
+	<th>Troop</th>
+	<th>Level</th>
+	<th>Quantity</th>
+	<th>Total</th>
+	<th>Subtract</th>
+  </tr>
+  
+  <tr>
+    <td>Barbarian</td>
+    <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+			<option value=5>5</option>
+			<option value=6>6</option>
+			<option value=7>7</option>
+		</select>
+	</td>
+	<td><input type="number" id="BarbarianQ" value="0"></td>
   </tr>
   <tr>
-    <td>Eve</td>
-    <td>Jackson</td> 
-    <td>94</td>
+    <td>Archer</td> 
+	    <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+			<option value=5>5</option>
+			<option value=6>6</option>
+			<option value=7>7</option>
+		</select>
+	</td>
+	<td><input type="number" id="ArcherQ" value="0"></td>
+    
+  </tr>
+  <tr>
+	<td>Giant</td>
+	    <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+			<option value=5>5</option>
+			<option value=6>6</option>
+			<option value=7>7</option>
+		</select>
+	</td>
+	<td><input type="number" id="GiantQ" value="0"></td>
+  </tr>
+  <tr>	<td>Goblin</td>
+      <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+			<option value=5>5</option>
+			<option value=6>6</option>
+		</select>
+	</td>
+	<td><input type="number" id="GoblinQ" value="0"></td>
+  </tr>
+  <tr><td>Wall Breaker</td>
+        <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+			<option value=5>5</option>
+			<option value=6>6</option>
+		</select>
+	</td>
+	<td><input type="number" id="WallBreakerQ" value="0"></td>
+  </tr>
+  <tr><td>Balloon</td>
+        <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+			<option value=5>5</option>
+			<option value=6>6</option>
+		</select>
+	</td>
+	<td><input type="number" id="BAlloonQ" value="0"></td>	
+  </tr>
+  <tr><td>Wizard</td>
+        <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+			<option value=5>5</option>
+			<option value=6>6</option>
+		</select>
+	</td>
+	<td><input type="number" id="WizardQ" value="0"></td>	
+  </tr>
+  <tr><td>Healer</td>
+        <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+		</select>
+	</td>
+	<td><input type="number" id="HealerQ" value="0"></td>	
+  </tr>
+  <tr><td>Dragon</td>
+          <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+			<option value=5>5</option>
+		</select>
+	</td>
+	<td><input type="number" id="DragonQ" value="0"></td>	
+  </tr>
+  <tr><td>P.E.K.K.A.</td>
+          <td>
+		<select>
+			<option value=1>1</option>
+			<option value=2>2</option>
+			<option value=3>3</option>
+			<option value=4>4</option>
+		</select>
+	</td>
+	<td><input type="number" id="PekkaQ" value="0"></td>
   </tr>
 </table>
 
+<script>
+function CalculateCost
+document.getElementById()
+
+</script>
 <?php
-echo "Hello world!!";
+echo "Ja hier staat nog steeds Hello World";
 
 ?>
